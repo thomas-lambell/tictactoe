29659509de26125f859eea7f27ed83e1c70fd877
thomas.lambell <thomas.lambell@gmail.com>
2020-04-14 16:15:17 +1000

    board now only allowing one click only

--- altScript.js
+++ altScript.js
@@ -1,5 +1,11 @@
 var playerTwoTurn = false;
 
+var squareClicked = []
+
+for (var i = 0; i < 9; i++){
+    squareClicked[i] = false;
+}
+
 //setup query listeners for the squares
 var square1 = document.querySelector("#square1");
 var square2 = document.querySelector("#square2");
@@ -12,97 +18,102 @@ var square8 = document.querySelector("#square8");
 var square9 = document.querySelector("#square9");
 
 square1.addEventListener("click", function(){
-    if(!playerTwoTurn){ 
+    if(!playerTwoTurn && !squareClicked[0]){ 
         this.textContent = "X";
         playerTwoTurn = true;
-    } else if(playerTwoTurn){
+        squareClicked[0] = true;
+    } else if(playerTwoTurn && !squareClicked[0]){
         this.textContent = "O";
         playerTwoTurn = false;
+        squareClicked[0] = true;
     }   
 })
+
 square2.addEventListener("click", function(){
-    if(!playerTwoTurn){ 
+    if(!playerTwoTurn && !squareClicked[1]){ 
         this.textContent = "X";
         playerTwoTurn = true;
-    } else if(playerTwoTurn){
+        squareClicked[1] = true;
+    } else if(playerTwoTurn && !squareClicked[1]){
         this.textContent = "O";
         playerTwoTurn = false;
-    }
+        squareClicked[1] = true;
+    }   
 })
 square3.addEventListener("click", function(){
-    if(!playerTwoTurn){ 
+    if(!playerTwoTurn && !squareClicked[2]){ 
         this.textContent = "X";
         playerTwoTurn = true;
-    } else if(playerTwoTurn){
+        squareClicked[2] = true;
+    } else if(playerTwoTurn && !squareClicked[2]){
         this.textContent = "O";
         playerTwoTurn = false;
-    }
+        squareClicked[2] = true;
+    }   
 })
 square4.addEventListener("click", function(){
-    if(!playerTwoTurn){ 
+    if(!playerTwoTurn && !squareClicked[3]){ 
         this.textContent = "X";
         playerTwoTurn = true;
-    } else if(playerTwoTurn){
+        squareClicked[3] = true;
+    } else if(playerTwoTurn && !squareClicked[3]){
         this.textContent = "O";
         playerTwoTurn = false;
-    }
+        squareClicked[3] = true;
+    }   
 })
 square5.addEventListener("click", function(){
-    if(!playerTwoTurn){ 
+    if(!playerTwoTurn && !squareClicked[4]){ 
         this.textContent = "X";
         playerTwoTurn = true;
-    } else if(playerTwoTurn){
+        squareClicked[4] = true;
+    } else if(playerTwoTurn && !squareClicked[4]){
         this.textContent = "O";
         playerTwoTurn = false;
-    }
+        squareClicked[4] = true;
+    }   
 })
 square6.addEventListener("click", function(){
-    if(!playerTwoTurn){ 
+    if(!playerTwoTurn && !squareClicked[5]){ 
         this.textContent = "X";
         playerTwoTurn = true;
-    } else if(playerTwoTurn){
+        squareClicked[5] = true;
+    } else if(playerTwoTurn && !squareClicked[5]){
         this.textContent = "O";
         playerTwoTurn = false;
-    }
+        squareClicked[5] = true;
+    }   
 })
 square7.addEventListener("click", function(){
-    if(!playerTwoTurn){ 
+    if(!playerTwoTurn && !squareClicked[6]){ 
         this.textContent = "X";
         playerTwoTurn = true;
-    } else if(playerTwoTurn){
+        squareClicked[6] = true;
+    } else if(playerTwoTurn && !squareClicked[6]){
         this.textContent = "O";
         playerTwoTurn = false;
-    }
+        squareClicked[6] = true;
+    }   
 })
 square8.addEventListener("click", function(){
-    if(!playerTwoTurn){ 
+    if(!playerTwoTurn && !squareClicked[7]){ 
         this.textContent = "X";
         playerTwoTurn = true;
-    } else if(playerTwoTurn){
+        squareClicked[7] = true;
+    } else if(playerTwoTurn && !squareClicked[7]){
         this.textContent = "O";
         playerTwoTurn = false;
-    }
+        squareClicked[7] = true;
+    }   
 })
 square9.addEventListener("click", function(){
-    if(!playerTwoTurn){ 
+    if(!playerTwoTurn && !squareClicked[8]){ 
         this.textContent = "X";
         playerTwoTurn = true;
-    } else if(playerTwoTurn){
+        squareClicked[8] = true;
+    } else if(playerTwoTurn && !squareClicked[8]){
         this.textContent = "O";
         playerTwoTurn = false;
-    }
+        squareClicked[8] = true;
+    }   
 })
-
-// function setup(){
-
-// for (var i = 0; i < squares.length; i++){
-//     squares[i].addEventListener("click", function(){
-//         if(!playerTwoTurn){ 
-//             this.textContent = "X";
-//             playerTwoTurn = true;
-//         } else if(playerTwoTurn){
-//             this.textContent = "O";
-//             playerTwoTurn = false;
-//         }
-// })}
-// }
