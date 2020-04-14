5b8d4f9a86460396959aa318e852ddb3576af7e9
Thomas Williams <tom.williams@workingmouse.com.au>
2020-04-14 22:25:04 +1000

    react

---
+++ react-tictactoe/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import TicTacToe from "./Components/TicTacToe/TicTacToe";
+
+export default function App() {
+	return (
+		<div className={'app'}>
+			<TicTacToe/>
+		</div>
+		);
+}
