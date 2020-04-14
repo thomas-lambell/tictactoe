45058dbb01aee3ad52bcf53e80ae3f35277c3fea
thomas.lambell <thomas.lambell@gmail.com>
2020-04-14 15:16:41 +1000

    can add x's and o's to the div

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+var playerTwoTurn = false;
+
 //setup query listeners for the squares
 var squares = document.querySelectorAll(".square");
 
+
 for (var i = 0; i < squares.length; i++){
     squares[i].addEventListener("click", function(){
-        alert("clicked");
+        if(!playerTwoTurn){
+        this.textContent = "X";
+        playerTwoTurn = true;
+        } else if(playerTwoTurn){
+            this.textContent = "O";
+            playerTwoTurn = false;
+        }
 })}
+
+
+
